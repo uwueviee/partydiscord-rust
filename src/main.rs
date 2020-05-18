263adfa467ef97b0306884f3484a8f9118c25269
Skye Viau <skye.viau@gmail.com>
2020-05-18 18:07:25 -0400

    Switch to warp and async version of serenity

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::env;
 
 use serenity::{
+    async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use tokio::time::delay_for;
+use std::time::Duration;
+
+use warp::Filter;
 
 struct Handler;
 
+#[async_trait]
 impl EventHandler for Handler {
-    fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, ctx: Context, msg: Message) {
         if msg.author.bot == true { return };
 
         /**
@@ -18,19 +24,19 @@ impl EventHandler for Handler {
         This is based off of the official Discord API documentation so it *should* be fine
         FYI 0x40000000 is MANAGE_EMOJIS on Discord's API (That's why I'm looking for it)
         **/
-        if  (msg.member(&ctx.cache).unwrap().permissions(&ctx.cache)
+        if  (msg.member(&ctx.cache).await.unwrap().permissions(&ctx.cache).await
                 .expect("permissions").bits & 0x40000000) == 0x40000000 {
 
                     // Upload default emoji pack
                     if msg.content == "+start" {
-                        if let Err(why) = msg.channel_id.say(&ctx.http, "<a:ultrafastparrot:405266489218826241> Uploading the emojis! <a:ultrafastparrot:405266489218826241>") {
+                        if let Err(why) = msg.channel_id.say(&ctx.http, "<a:ultrafastparrot:405266489218826241> Uploading the emojis! <a:ultrafastparrot:405266489218826241>").await {
                             println!("Error sending message: {:?}", why);
                         }
                     }
 
                     // Upload chosen emojis
                     if msg.content == "+choose" {
-                        if let Err(why) = msg.channel_id.say(&ctx.http, "<a:ultrafastparrot:405266489218826241> Uploading the emojis! <a:ultrafastparrot:405266489218826241>") {
+                        if let Err(why) = msg.channel_id.say(&ctx.http, "<a:ultrafastparrot:405266489218826241> Uploading the emojis! <a:ultrafastparrot:405266489218826241>").await {
                             println!("Error sending message: {:?}", why);
                         }
                     }
@@ -38,7 +44,7 @@ impl EventHandler for Handler {
                     // Show available emojis
                     if msg.content == "+showemojis" {
                         // Dynamically load all filenames in /emoji/ and print them here
-                        if let Err(why) = msg.channel_id.say(&ctx.http, "") {
+                        if let Err(why) = msg.channel_id.say(&ctx.http, "").await {
                             println!("Error sending message: {:?}", why);
                         }
                     }
@@ -48,26 +54,80 @@ impl EventHandler for Handler {
             // To be removed later, just for testing shards
             println!("Shard {}", ctx.shard_id);
 
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    fn ready(&self, ctx: Context, ready: Ready) {
-        println!("{} shard {} is connected!", ready.user.name, ctx.shard_id);
+    async fn ready(&self, _: Context, ready: Ready) {
+        if let Some(shard) = ready.shard {
+            // Note that array index 0 is 0-indexed, while index 1 is 1-indexed.
+            //
+            // This may seem unintuitive, but it models Discord's behaviour.
+            println!(
+                "{} is connected on shard {}/{}!",
+                ready.user.name,
+                shard[0],
+                shard[1],
+            );
+        }
     }
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     // Download the new parrots *BEFORE* the bot or API starts
+    download_parrots();
+    // Start the API and Discord bot
+    futures::join!(start_api(), start_bot());
+}
 
+async fn download_parrots() {
+    println!("DOWNLOADING PARROTS");
+
+    println!("DOWNLOADED PARROTS");
+}
+
+async fn start_api() {
+    println!("API STARTING");
+    let status = warp::path!("status")
+        .map(|| "OK");
+
+    warp::serve(status)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+    println!("API STARTED");
+}
+
+async fn start_bot() {
+    println!("STARTING DISCORD BOT");
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
+    let mut client = Client::new(&token).event_handler(Handler).await.expect("Err creating client");
 
-    if let Err(why) = client.start_shards(12) {
+    let manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        loop {
+            delay_for(Duration::from_secs(30)).await;
+
+            let lock = manager.lock().await;
+            let shard_runners = lock.runners.lock().await;
+
+            for (id, runner) in shard_runners.iter() {
+                println!(
+                    "Shard ID {} is {} with a latency of {:?}",
+                    id,
+                    runner.stage,
+                    runner.latency,
+                );
+            }
+        }
+    });
+
+    if let Err(why) = client.start_shards(12).await {
         println!("Client error: {:?}", why);
     }
+    println!("DISCORD BOT STARTED");
 }
